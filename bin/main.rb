a45b64e7aa8f881a7845b5e4baaa63371b4fe5c5
vzdrizhni <vzdrizhni@gmail.com>
2020-03-18 15:06:58 +0500

    display_field method added

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
 puts 'Hello World!'
+
+class Interface
+    def initialize()
+        @field = ["__", "__", "__", "__", "__", "__", "__", "__", "__"]
+    end
+
+    def display_field
+       @field.each_with_index do |field, index|
+        print "| #{field} |"
+        if (index == 2 || index == 5 || index == 8) && index != 0
+            puts "\n"
+        end
+    end
+    end
+end
+
+new_game = Interface.new
+new_game.display_field
