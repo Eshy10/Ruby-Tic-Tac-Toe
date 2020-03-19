2d2f618c3814aad7bc12b5d06bcd03f7e6647010
modupe <adewalebose15@gmail.com>
2020-03-19 13:42:12 +0100

    Fixed linters errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
-require_relative "./game_logic.rb"
-
+require_relative './game_logic.rb'
 class Interface
-
   attr_reader :players
 
   def initialize()
@@ -33,20 +31,20 @@ class Interface
     a = Logic.new
     game_over = false
     until game_over == true
-    @players.each do |player|
-      puts "#{player} pick a number between 0-8"
-      input = gets.chomp
-      input = a.is_number_valid?(input.to_i, player)
-      if player.include?(@players[0])
-        a.occupied?(@field, input.to_i, 'X')
-        display_field
-      return game_over = true if a.winning_condition(@field, @players[0])
-      else
-        a.occupied?(@field, input.to_i, 'O')
-        display_field
-      return game_over = true if a.winning_condition(@field, players[1])
+      @players.each do |player|
+        puts "#{player} pick a number between 0-8"
+        input = gets.chomp
+        input = a.number_valid?(input.to_i, player)
+        if player.include?(@players[0])
+          a.occupied?(@field, input.to_i, 'X')
+          display_field
+          return game_over = true if a.winning_condition(@field, @players[0])
+        else
+          a.occupied?(@field, input.to_i, 'O')
+          display_field
+          return game_over = true if a.winning_condition(@field, players[1])
+        end
       end
-    end
     end
   end
 
